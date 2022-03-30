526fbdbca54d00cb15717bf7b6e0f805f0b58f5a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 23:44:56 +0900

    #15 Split components - Chuyển / cắt sang 1 component riêng khác để tái sử dụng

--- src/views/Example/AddComponent.js
+++ src/views/Example/AddComponent.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 
 class AddComponent extends Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+    }
+
+    handleChangeFirstName = (event) => {
+        this.setState(
+            {
+                firstName: event.target.value
+            }
+        )
+    }
+
+    handleChangeLastName = (event) => {
+        this.setState(
+            {
+                lastName: event.target.value
+            }
+        )
+    }
+
     render() {
         return (
-            <div>
-                Hello Add Component
-            </div>
+            <form>
+                <label htmlFor="fname">First Name</label><br />
+                <input
+                    type="text"
+                    value={this.state.firstName}
+                    onChange={(event) => this.handleChangeFirstName(event)}
+                />
+                <br />
+                <label htmlFor="lname">Last Name</label><br />
+                <input
+                    type="text"
+                    value={this.state.lastName}
+                    onChange={(event) => this.handleChangeLastName(event)}
+                />
+                <br />
+                <input
+                    type="submit"
+                    onClick={(event) => this.handleSubmit(event)}
+                />
+            </form>
         );
     }
 }
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -5,8 +5,6 @@ import AddComponent from './AddComponent'
 class MyComponent extends React.Component {
 
     state = {
-        firstName: "",
-        lastName: "",
         arrJobs: [
             {
                 id: 'abcJob1',
@@ -26,21 +24,6 @@ class MyComponent extends React.Component {
         ],
     }
 
-    handleChangeFirstName = (event) => {
-        this.setState(
-            {
-                firstName: event.target.value
-            }
-        )
-    }
-
-    handleChangeLastName = (event) => {
-        this.setState(
-            {
-                lastName: event.target.value
-            }
-        )
-    }
 
     handleSubmit = (event) => {
         event.preventDefault()
@@ -51,30 +34,7 @@ class MyComponent extends React.Component {
         return (
             <>
                 <AddComponent />
-                <form>
-                    <label htmlFor="fname">First Name</label><br />
-                    <input
-                        type="text"
-                        value={this.state.firstName}
-                        onChange={(event) => this.handleChangeFirstName(event)}
-                    />
-                    <br />
-                    <label htmlFor="lname">Last Name</label><br />
-                    <input
-                        type="text"
-                        value={this.state.lastName}
-                        onChange={(event) => this.handleChangeLastName(event)}
-                    />
-                    <br />
-                    <input
-                        type="submit"
-                        onClick={(event) => this.handleSubmit(event)}
-                    />
-                </form>
                 <ChildComponent
-                    name={this.state.firstName}
-                    age={'25'}
-                    address={'Japan'}
                     arrJobs={this.state.arrJobs}
                 />
             </>
