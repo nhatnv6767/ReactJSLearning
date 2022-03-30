ea93bfce3e256efb477243af20477ae3ae151b26
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 20:53:29 +0900

    #9 Form in ReactJS - create form 2

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -3,15 +3,17 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
+        firstName: "",
+        lastName: "",
     }
     render() {
         return (
             <>
                 <form>
                     <label for="fname">First Name</label><br />
-                    <input type="text" id="fname" name="fname" value="John" /><br />
+                    <input type="text" value="" /><br />
                     <label for="lname">Last Name</label><br />
-                    <input type="text" id="lname" name="lname" value="Doe" /><br />
+                    <input type="text" value="" /><br />
                     <input type="submit" value="Submit" />
                 </form>
             </>
