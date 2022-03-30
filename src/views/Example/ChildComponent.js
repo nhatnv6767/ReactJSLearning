ef5975ceccaeb409116489b3356410cc31b53a2b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 23:00:41 +0900

    #13 Stateless/StateFull components - cach dung prop trong truong hop nay

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,31 +1,22 @@
 import React from "react";
 
-// class ChildComponent extends React.Component {
-
-//     render() {
-//         let { name, age, address, arrJobs } = this.props
-//         // console.log('>>>> Check props: ', this.props)
-//         return (
-//             <>
-//                 <div className="job-lists">
-//                     {
-//                         arrJobs.map((item, index) => {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title} - {item.salary}
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//             </>
-//         )
-//     }
-// }
-
 const ChildComponent = (props) => {
-    console.log('>>>> Check child props', props)
-    return (<div>Hello Arrow function</div>)
+    let { arrJobs } = props;
+    return (
+        <>
+            <div className="job-lists">
+                {
+                    arrJobs.map((item, index) => {
+                        return (
+                            <div key={item.id}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
 
 export default ChildComponent;
