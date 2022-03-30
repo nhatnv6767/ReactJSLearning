0dfde1d74ece87e82b952f291da74497d40aff5c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 14:08:58 +0900

    #5 State - JSX return block

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -3,15 +3,21 @@ import React from "react";
 class MyComponent extends React.Component {
 
     /**
-     * 
+     * JSX => return block
      */
     render() {
         let name = 'None';
 
         return (
-            <div>
-                {console.log('My name in console log is: ', name)}
-                Hello Component, My name is {name}
+            <div className="divT">
+                <div className="first">
+                    {console.log('My name in console log is: ', name)}
+                    Hello Component, My name is {name}
+                </div>
+                <div className="second">
+                    div 2
+                </div>
+
             </div>
         )
     }
