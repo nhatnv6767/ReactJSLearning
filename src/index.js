a2e6539546fa4f322f9c06c8ea2dc01cc7da668a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 21:30:14 +0900

    some fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client'
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import './styles/global.scss';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+// );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
