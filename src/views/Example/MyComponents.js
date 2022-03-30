2b13bde92ec6249a089e5425c7484be5ade19db2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 14:29:30 +0900

    #5 State - State on change realtime

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -3,19 +3,29 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
-        name: 'None State',
+        name: 'CBBang',
         channel: 'Balangh'
     }
 
     /**
      * JSX => return block
      */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
 
         return (
             <>
                 <div className="first">
-                    Hello Component, My name is {this.state['name']}
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    <br />
+                    My name is {this.state['name']}
                 </div>
                 <div className="second">
                     My Channel is {this.state.channel}
