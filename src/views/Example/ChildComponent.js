a19d25a2c59ed44070476a406a326e81cc669c5b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 23:13:46 +0900

    #14 Conditional Output - Chuyen ve class

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,24 +1,50 @@
 import React from "react";
 
-const ChildComponent = (props) => {
-    let { arrJobs } = props;
-    return (
-        <>
-            <div className="job-lists">
-                {
-                    arrJobs.map((item, index) => {
-                        if (item.salary >= 500) {
+
+class ChildComponent extends React.Component {
+
+    render() {
+        let { arrJobs } = this.props
+        // console.log('>>>> Check props: ', this.props)
+        return (
+            <>
+                <div><button>Show</button></div>
+                <div className="job-lists">
+                    {
+                        arrJobs.map((item, index) => {
                             return (
                                 <div key={item.id}>
-                                    {item.title} - {item.salary}$
+                                    {item.title} - {item.salary}
                                 </div>
                             )
-                        }
-                    })
-                }
-            </div>
-        </>
-    )
+                        })
+                    }
+                </div>
+                <div><button>Hide</button></div>
+            </>
+        )
+    }
 }
+
+// const ChildComponent = (props) => {
+//     let { arrJobs } = props;
+//     return (
+//         <>
+//             <div className="job-lists">
+//                 {
+//                     arrJobs.map((item, index) => {
+//                         if (item.salary >= 500) {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}$
+//                                 </div>
+//                             )
+//                         }
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
+// }
 
 export default ChildComponent;
