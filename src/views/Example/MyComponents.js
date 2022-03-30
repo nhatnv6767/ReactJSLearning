1249359e0ad75fde824b2f74bf727288c3c822ce
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 13:44:15 +0900

    components

---
+++ src/views/Example/MyComponents.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello component My Max</div>
+        )
+    }
+}
+
+export default MyComponent;
