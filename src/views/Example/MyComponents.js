892cefc71c9b535b28e2bad4a32cda2e76cfb7d5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 20:43:41 +0900

    #9 Form in ReactJS

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -3,43 +3,16 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
-        name: '',
-        channel: 'Balangh'
+
     }
 
-    /**
-     * JSX => return block
-     */
 
-    handleOnChangeName = (event) => {
-        // console.log(event.target.value, 'event target: ', event.target, 'event object: ', event)
-        // Mỗi 1 lần gọi hàm this.setState thì React sẽ nhận được tín hiệu 
-        // để cho nó biết nó cần phải set lại biến state, đồng thời gọi lại render() để re-render
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleClickButton = () => {
-        alert('Click meeeee')
-    }
     render() {
-        console.log('>>> call render: ', this.state)
+
         return (
             <>
-                <div className="first">
-                    <input value={this.state.name} type="text"
-                        onChange={(event) => this.handleOnChangeName(event)}
-                    />
-                    <br />
-                    My name is {this.state['name']}
-                </div>
-                <div className="second">
-                    My Channel is {this.state.channel}
-                </div>
-
-                <div className="third">
-                    <button onClick={() => { this.handleClickButton() }}>Click me</button>
+                <div>
+                    Hello Form
                 </div>
             </>
 
