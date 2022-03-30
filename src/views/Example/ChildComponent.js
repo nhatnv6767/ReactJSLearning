9bddaeaa7d5402ce21b2992273923ffe8410e96b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 22:54:06 +0900

    #13 Stateless/StateFull components

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,26 +1,30 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
+// class ChildComponent extends React.Component {
 
-    render() {
-        let { name, age, address, arrJobs } = this.props
-        // console.log('>>>> Check props: ', this.props)
-        return (
-            <>
-                <div className="job-lists">
-                    {
-                        arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </>
-        )
-    }
+//     render() {
+//         let { name, age, address, arrJobs } = this.props
+//         // console.log('>>>> Check props: ', this.props)
+//         return (
+//             <>
+//                 <div className="job-lists">
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+const ChildComponent = () => {
+    return (<div>Hello Arrow function</div>)
 }
 
 export default ChildComponent;
