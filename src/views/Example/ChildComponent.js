2fa75798084d42bf435c193b113e624820dc9f84
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 23:36:09 +0900

    .....................

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -45,25 +45,5 @@ class ChildComponent extends React.Component {
     }
 }
 
-// const ChildComponent = (props) => {
-//     let { arrJobs } = props;
-//     return (
-//         <>
-//             <div className="job-lists">
-//                 {
-//                     arrJobs.map((item, index) => {
-//                         if (item.salary >= 500) {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title} - {item.salary}$
-//                                 </div>
-//                             )
-//                         }
-//                     })
-//                 }
-//             </div>
-//         </>
-//     )
-// }
 
 export default ChildComponent;
