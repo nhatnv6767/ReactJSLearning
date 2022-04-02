309be45e743401ec37298205910897909d1bf0cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-02 18:22:45 +0900

    #21. Todos app: Add new todo: Con cung can tao 1 state

--- src/views/Todos/AddTodo.js
+++ src/views/Todos/AddTodo.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class AddTodo extends React.Component {
+    state = {
+        title: ''
+    }
     render() {
         return (
             <div className="add-todo">
