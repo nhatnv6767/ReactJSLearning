3b981c6cc6844a97e408d003d69c9fe8946e62b6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-02 16:41:29 +0900

    #21. Todos app: Read data

--- src/views/Todos/ListToDo.js
+++ src/views/Todos/ListToDo.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class ListToDo extends React.Component {
+    state = {
+        listTodos: [
+            { id: 'todo1', title: 'Doing homework' },
+            { id: 'todo2', title: 'Making video' },
+            { id: 'todo3', title: 'Fixing bugs' },
+        ],
+    }
     render() {
+        let { listTodos } = this.state;
         return (
             <div className="list-todo-container">
                 <div className="add-todo">
