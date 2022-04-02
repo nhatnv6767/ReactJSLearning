84bb8387b2d35a79e92f7552e5e3f81fdc02fc8d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-02 16:38:24 +0900

    #21. Todos app: Render List todo

--- src/views/Todos/ListToDo.js
+++ src/views/Todos/ListToDo.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class ListToDo extends React.Component {
     render() {
         return (
-            <div>
-                Hello List To Do
+            <div className="list-todo-container">
+                <div className="add-todo">
+                    <input type="text" />
+                    <button type="button">Add</button>
+                </div>
+                <div className="list-todo-content">
+
+                </div>
             </div>
         );
     }
