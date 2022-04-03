e39736288376e5ad5e02f426c06aa79726a78dfa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 23:50:05 +0900

    #23 Links NavLinks exact

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -8,9 +8,9 @@ class Nav extends Component {
     render() {
         return (
             <div className="topnav">
-                <Link to="/">Home</Link>
-                <Link to="/todo">Todos</Link>
-                <Link to="/about">About</Link>
+                <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
+                <NavLink to="/todo" activeClassName="active">Todos</NavLink>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
             </div>
         );
     }
