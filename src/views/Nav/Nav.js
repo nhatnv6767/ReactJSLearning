2d844cc410118aed42fda021d55598d6afce6851
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 23:05:33 +0900

    #22 React routers: Nav

---
+++ src/views/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Navigation
+            </div>
+        );
+    }
+}
+
+export default Nav;
