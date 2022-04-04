16d61a822185c9c59619126efc4de08ee85f0dba
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 22:14:49 +0900

    #26 Axios: scss

--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './ListUser.scss';
 
 class ListUser extends Component {
     state = {
@@ -14,8 +15,24 @@ class ListUser extends Component {
     // khi chạy sẽ chạy vào render lần 1 và sau đó sẽ chạy vào componentDidMount
     render() {
         return (
-            <div>
+            <div className="list-user-container">
+                <div className="title">
+                    Fetch all list users
+                </div>
 
+                <div className="list-user-content">
+                    <div className="child">
+                        1 - None - Bulang
+                    </div>
+
+                    <div className="child">
+                        1 - None - Bulang
+                    </div>
+
+                    <div className="child">
+                        1 - None - Bulang
+                    </div>
+                </div>
             </div>
         );
     }
