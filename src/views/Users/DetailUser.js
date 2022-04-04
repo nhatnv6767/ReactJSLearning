2e376b1421b06ec35c70be1ce8e6890eb8f29f35
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 22:51:58 +0900

    #27 Route parameters - 3

--- src/views/Users/DetailUser.js
+++ src/views/Users/DetailUser.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
+import axios from 'axios'
 
 class DetailUser extends Component {
-    componentDidMount() {
+    state = {
+        user: {}
+    }
+    async componentDidMount() {
+        if (this.props.match && this.props.match.params) {
+            let id = this.props.match.params.id
+            let res = await axios.get(`https://reqres.in/api/users/${id}`)
+            console.log(">>> Check res: ", res)
+        }
     }
     render() {
         console.log(">>> Check props: ", this.props)
