bea23a8b371a959f1bf3d3ed88722b660fd9aeca
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 21:56:18 +0900

    #26 Axios: Get by axios

--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ListUser extends Component {
+    componentDidMount() {
+        axios.get('https://reqres.in/api/users?page=2')
+            .then(res => {
+                console.log(">>>> Check res: ", res);
+            })
+    }
+    // khi chạy sẽ chạy vào render lần 1 và sau đó sẽ chạy vào componentDidMount
     render() {
         return (
             <div>
