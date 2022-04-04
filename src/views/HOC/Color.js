940879a6d7369476f3fbd1e34d8a15b480f580b5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 21:29:09 +0900

    #25 HOC - explain

--- src/views/HOC/Color.js
+++ src/views/HOC/Color.js
@@ -14,6 +14,11 @@ function getRandomColor() {
 // truyền vào 1 component thay vì là 1 biến như bình thường
 const Color = (WrappedComponent) => {
     const colorRandom = getRandomColor()
+    // Khi return, để cho thằng React hiểu là chúng ta đang viết 1 component
+    // thí bắt buộc phải truyền props vào nó
+    // nhận đầu vào như thế nào thì phải nhả ra như vậy
+    // Khi nhận đầu vào là Home component, thì props là props của Home component
+    // nó có cái gì thì phải truyền y nguyên lại, vì vậy phải bắt buộc có props => rule của React
     return (props) => (
         <div style={{ color: colorRandom }}>
             <WrappedComponent {...props} />
