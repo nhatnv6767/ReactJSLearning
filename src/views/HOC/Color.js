ef5a107872c18e3532e33679a099dcf2c5794d5a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 21:16:30 +0900

    #25 HOC - Random color

--- src/views/HOC/Color.js
+++ src/views/HOC/Color.js
@@ -2,6 +2,14 @@
 
 import React from 'react'
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+}
+
 // truyền vào 1 component thay vì là 1 biến như bình thường
 export default Color = (WrappedComponent) => {
     return (
