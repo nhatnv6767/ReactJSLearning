2b019ed6fb128c38d0d6b69a3e9bd4425929bf4d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-04 22:33:36 +0900

    #27 Route parameters - Sử dụng tham số trên URL

---
+++ src/views/Users/DetailUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DetailUser extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default DetailUser;
