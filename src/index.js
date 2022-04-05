f47f63f73a371e0f40cbf6db9b602bd9a15b8bcd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-05 22:01:10 +0900

    #32 Setup Redux - tạo 1 thằng công nhân - 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import * as ReactDOMClient from 'react-dom/client'
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import './styles/global.scss';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
 
 
 ReactDOM.render(
